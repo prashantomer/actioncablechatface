ad4693b05075e84817450532f7daae8257509229
Shankar <shankar.yuvasoft15@gmail.com>
2017-05-15 20:16:17 +0530

    form commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :messages
+  # has_many :messages
 	has_many :conversations, foreign_key: :sender_id, dependent: :destroy
 	has_one :user_profile, dependent: :destroy
 
